d7719ce98b372548d6c2bf126b2b2b3d134d4591
kureungkureung <terry9611@naver.com>
2022-10-27 21:10:08 +0900

    test(OperatorTest): 곱셈 기능

--- src/test/java/io/github/kureung/book/OperatorTest.java
+++ src/test/java/io/github/kureung/book/OperatorTest.java
@@ -13,4 +13,8 @@ class OperatorTest {
         assertThat(Operator.ADDITION.calculatedResult(List.of(1, 2))).isEqualTo(3);
     }
 
+    @Test
+    void 곱할_수_있다() {
+        assertThat(Operator.MULTIPLICATION.calculatedResult(List.of(1, 2))).isEqualTo(2);
+    }
 }
