b7357d8fa620d8541c6f0e9e41b25115726b8f80
kureungkureung <terry9611@naver.com>
2022-10-27 21:08:50 +0900

    feat(Operator): 덧셈 기능

---
+++ src/main/java/io/github/kureung/book/Operator.java
@@ -0,0 +1,20 @@
+package io.github.kureung.book;
+
+import java.util.List;
+import java.util.function.BinaryOperator;
+
+public enum Operator {
+    ADDITION((a, b) -> a + b);
+
+    private final BinaryOperator<Integer> formula;
+
+    Operator(BinaryOperator<Integer> formula) {
+        this.formula = formula;
+    }
+
+    public int calculatedResult(List<Integer> numbers) {
+        return numbers.stream()
+                .reduce(formula)
+                .orElse(0);
+    }
+}
