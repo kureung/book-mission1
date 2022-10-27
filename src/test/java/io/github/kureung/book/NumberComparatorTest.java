b857e159435bbdae5cecf16ef7d80ec1838e05f3
kureungkureung <terry9611@naver.com>
2022-10-27 21:24:45 +0900

    test(NumberComparatorTest): 곱셈과 덧셈 중 더 큰 값 반환

---
+++ src/test/java/io/github/kureung/book/NumberComparatorTest.java
@@ -0,0 +1,14 @@
+package io.github.kureung.book;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class NumberComparatorTest {
+
+    @Test
+    void 곱셈과_덧셈_중_더_큰_값을_반환한다() {
+        NumberComparator numberComparator = new NumberComparator(97);
+        assertThat(numberComparator.greaterResult()).isEqualTo(63);
+    }
+}
