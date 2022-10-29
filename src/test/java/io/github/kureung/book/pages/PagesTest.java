01a621e5598ed6f31e97bde8167c9ccec3f75ae7
kureungkureung <terry9611@naver.com>
2022-10-29 14:29:43 +0900

    test(PagesTest): 승부 판별 기능

--- src/test/java/io/github/kureung/book/pages/PagesTest.java
+++ src/test/java/io/github/kureung/book/pages/PagesTest.java
@@ -1,6 +1,11 @@
 package io.github.kureung.book.pages;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,4 +16,20 @@ class PagesTest {
         Pages pages = new Pages(97, 98);
         assertThat(pages.greaterResult()).isEqualTo(72);
     }
+
+
+    @MethodSource
+    @ParameterizedTest
+    void 승부를_결과를_반환할_수_있다(Pages pages, Pages otherPages, MatchResult matchResult) {
+        assertThat(pages.matchResult(otherPages)).isEqualTo(matchResult);
+    }
+
+    private static Stream<Arguments> 승부를_결과를_반환할_수_있다() {
+        return Stream.of(
+                Arguments.of(new Pages(97, 98), new Pages(197, 198), MatchResult.DRAW),
+                Arguments.of(new Pages(97, 98), new Pages(22, 23), MatchResult.WIN),
+                Arguments.of(new Pages(22, 23), new Pages(97, 98), MatchResult.LOSE)
+        );
+    }
+
 }
