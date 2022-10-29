060d17d91f4ac39447a80d7f95f10e0e62a00232
kureungkureung <terry9611@naver.com>
2022-10-29 14:54:57 +0900

    feat(MatchResult): 결과에 따른 정수 값 반환

--- src/main/java/io/github/kureung/book/pages/MatchResult.java
+++ src/main/java/io/github/kureung/book/pages/MatchResult.java
@@ -1,9 +1,19 @@
 package io.github.kureung.book.pages;
 
 public enum MatchResult {
-    WIN,
-    LOSE,
-    DRAW,
+    WIN(1),
+    LOSE(2),
+    DRAW(0),
     ;
+
+    private final int value;
+
+    MatchResult(int value) {
+        this.value = value;
+    }
+
+    public int value() {
+        return value;
+    }
 
 }
